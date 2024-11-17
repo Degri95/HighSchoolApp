5fc0005a2bfe48d208e5a7f0f4cc2fc837923e5e
david.egri95@gmail.com <david.egri95@gmail.com>
2024-11-17 14:23:16 -0800

    Adding pages and Exam DAL

--- HighSchoolApp.DAL/ExamAdapter.cs
+++ HighSchoolApp.DAL/ExamAdapter.cs
@@ -3,11 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Dapper;
 
 namespace HighSchoolApp.DAL
 {
     internal class ExamAdapter
     {
         private string CONN_STRING = @"Data Source=C:\Sqlite\School.db;Version=3;";
+
+        public IEnumerable<Exam> GetAllExams()
+        {
+            string sql = @"SELECT * FROM Exam";
+
+            using (SqliteConnection connection = new SqliteConnection(CONN_STRING))
+            {
+                return connection.Query<Exam>(sql);
+            }
+        }
+
+        public Exam GetExamById(int examId)
+        {
+            string sql = @"SELECT * FROM Exam WHERE ExamId = @ExamId";
+
+            using (SqliteConnection connection = new SqliteConnection(CONN_STRING))
+            {
+                return connection.QueryFirst<Exam>(sql, new { ExamId = examId });
+            }
+        }
+
+        public bool InsertExam(Exam exam)
+        {
+            string sql = @"INSERT INTO Exam (StudentId, Score) VALUES (@StudentId, @Score)";
+
+            using (SqliteConnection connection = new SqliteConnection(CONN_STRING))
+            {
+                int rowsAffected = connection.Execute(sql, exam);
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool UpdateExam(Exam exam)
+        {
+            string sql = @"UPDATE Exam SET StudentId = @StudentId, Score = @Score WHERE ExamId = @ExamId";
+
+            using (SqliteConnection connection = new SqliteConnection(CONN_STRING))
+            {
+                int rowsAffected = connection.Execute(sql, exam);
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool DeleteExam(int examId)
+        {
+            string sql = "DELETE FROM Exam WHERE ExamId = @ExamId";
+
+            using (SqliteConnection connection = new SqliteConnection(CONN_STRING))
+            {
+                int rowsAffected = connection.Execute(sql, new { ExamId = examId });
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
---
+++ HighSchoolApp/Pages/Student/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HighSchoolApp.Pages.Student
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
