0c15450d17d834951b6c45830de042728bfda6ed
Degri95 <david.egri95@gmail.com>
2024-11-19 18:13:37 -0800

    Updated privacy and exam deletion page

--- HighSchoolApp/Pages/Exam/Delete.cshtml.cs
+++ HighSchoolApp/Pages/Exam/Delete.cshtml.cs
@@ -1,3 +1,4 @@
+using HighSchoolApp.DAL;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,32 @@ namespace HighSchoolApp.Pages.Exam
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private IExamAdapter _examAdapter;
+        public DeleteModel(IExamAdapter examAdapter)
         {
+            _examAdapter = examAdapter;
+        }
+        public int Id { get; set; }
+        public bool IsSuccess { get; set; }
+        public void OnGet(int id = 0)
+        {
+            Id = id;
+            if (id > 0) 
+            { 
+                bool isDelete = _examAdapter.DeleteExam(id);
+                if (isDelete)
+                {
+                    IsSuccess = true;
+                }
+                else
+                {
+                    IsSuccess = false;
+                }
+            }
+            else
+            {
+                IsSuccess = false;
+            }
         }
     }
 }
